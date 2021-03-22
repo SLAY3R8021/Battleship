b44bf99df92f555a17853ec5b56328cb8758b386
SLAY3R8021 <mpoppe8021@gmail.com>
2021-03-22 09:44:28 -0500

    Added the rest of the creating ships function.

--- game.py
+++ game.py
@@ -1,7 +1,3 @@
-#Creating an Intro and setting up the game
-print("\n---LET'S PLAY BATTLESHIP---")
-print("\nFirst let's create out battleships. Here is your grid.\n")
-
 class Grid:
     def __init__(self, size):
         self.size = size
@@ -45,12 +41,27 @@ class Grid:
                 for t in range(self.size + 1):
                     val = temp[t]
                     string += val
-                print(string)
+                print(string)while pmove_count <= ship_size:
+                            if prow[pcol] == "     /":
+                                continue
+                            else:
+                                return print("\nShips overlap. Please choose another spot or different direction.")
+                            ppot -= 1
+                            prow = self.ship[ppot]
+                            pmove_count += 1
     
     def create_ship(self, location, direction, ship_size, number):
         alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
         for j in range(1, len(alpha) + 1):
             if str(location[0]) == str(alpha[j - 1]):
+                col = int(location[1])
+                pot = j
+                row = self.ship[pot]
+                move_count = 1
+                pcol = col
+                ppot = pot
+                prow = row
+                pmove_count = move_count
                 if direction == "U" and (j - ship_size) < 0:
                     return print("\nInvalid placement. Try different spot.")
                 elif direction == "R" and (int(location[1]) + ship_size - 1) > self.size:
@@ -58,27 +69,49 @@ class Grid:
                 elif direction == "D" and (j + ship_size - 1) > self.size:
                     return print("\nInvalid placement. Try different spot.")
                 elif direction == "L" and (int(location[1]) - ship_size) < 0:
-                    return print("\nInvalid placement. Try different spot."
+                    return print("\nInvalid placement. Try different spot.")
                 else:
-                    col = int(location[1])
-                    pot = j
-                    row = self.ship[pot]
-                    row[col] = "  {}  /".format(number)
-                    self.count += ship_size
-                    move_count = 1
                     if direction == "L":
+                        while pmove_count <= ship_size:
+                            if prow[pcol] == "     /":
+                                continue
+                            else:
+                                return print("\nShips overlap. Please choose another spot or different direction.")
+                            pcol -= 1
+                            pmove_count += 1
+                        row[col] = "  {}  /".format(number)
+                        self.count += ship_size
                         while move_count < ship_size:
                             col -= 1
                             row[col] = "  {}  /".format(number)
                             move_count += 1
                         self.ship[pot] = row
                     elif direction == "R":
+                        while pmove_count <= ship_size:
+                            if prow[pcol] == "     /":
+                                continue
+                            else:
+                                return print("\nShips overlap. Please choose another spot or different direction.")
+                            pcol += 1
+                            pmove_count += 1
+                        row[col] = "  {}  /".format(number)
+                        self.count += ship_size
                         while move_count < ship_size:
                             col += 1
                             row[col] = "  {}  /".format(number)
                             move_count += 1
                         self.ship[pot] = row
                     elif direction == "U":
+                        while pmove_count <= ship_size:
+                            if prow[pcol] == "     /":
+                                continue
+                            else:
+                                return print("\nShips overlap. Please choose another spot or different direction.")
+                            ppot -= 1
+                            prow = self.ship[ppot]
+                            pmove_count += 1
+                        row[col] = "  {}  /".format(number)
+                        self.count += ship_size
                         self.ship[pot] = row
                         while move_count < ship_size:
                             pot -= 1
@@ -86,6 +119,27 @@ class Grid:
                             row[col] = "  {}  /".format(number)
                             self.ship[pot] = row
                             move_count += 1
+                    elif direction == "D":
+                        while pmove_count <= ship_size:
+                            if prow[pcol] == "     /":
+                                continue
+                            else:
+                                return print("\nShips overlap. Please choose another spot or different direction.")
+                            ppot += 1
+                            prow = self.ship[ppot]
+                            pmove_count += 1
+                        row[col] = "  {}  /".format(number)
+                        self.count += ship_size
+                        self.ship[pot] = row
+                        while move_count < ship_size:
+                            pot += 1
+                            row = self.ship[pot]
+                            row[col] = "  {}  /".format(number)
+                            self.ship[pot] = row
+                            move_count += 1
+                    else:
+                        return print("\nDirection or location is invalid. Please enter a new spot.")
+
 
 
 grid = Grid(10)
